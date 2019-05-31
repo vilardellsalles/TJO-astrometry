bc4bc18bf2a56285f4f4f36f9b7b3e718cf00984
Francesc Vilardell <vilardell@ieec.cat>
2019-05-31 16:22:44 +0200

    Preliminary  pipeline that uses database to work with photometry

--- README.md
+++ README.md
@@ -7,15 +7,53 @@ This repo contains the steps followed to reduce the data from the TJO proposal
 spectroscopy". The data for this project can be summarized in the following 
 table:
 
-| object     | instrume | filter | Number of images |
-| ---------- | -------- | ------ | -------- |
-| 12 Oph     | MEIA     | B      |       56 |
-| 12 Oph Ref | MEIA     | B      |        3 |
-| HD 135599  | MEIA     | B      |       78 |
-| HD 180617  | MEIA     | B      |       19 |
-| HD 98712   | MEIA     | B      |       78 |
-| OT Ser     | MEIA2    | V      |      534 |
-| V2306 Oph  | MEIA2    | V      |      150 |
+<div align="center">
+
+| object     | instrume | filter | focus range | images |
+| ---------- | -------- | ------ | ----------- | ------ |
+| 12 Oph     | MEIA     | B      | 4464 - 4474 |    56 |
+| 12 Oph Ref | MEIA     | B      | 4466 - 4467 |     3 |
+| HD 135599  | MEIA     | B      | 3935 - 4054 |    78 |
+| HD 180617  | MEIA     | B      | 3217 - 3463 |    19 |
+| HD 98712   | MEIA     | B      | 3440 - 4231 |    78 |
+| OT Ser     | MEIA2    | V      | 7859 - 8884 |   534 |
+| V2306 Oph  | MEIA2    | V      | 7865 - 8871 |   150 |
+
+</div>
+
+## Data issues
+
+I have observed the following issues regarding data quality:
+
+* **HD 98712.** The focus position for this object is not constant and several 
+images were obtained with a different focus:
+
+<div align="center">
+
+| focus | images   |
+| ----- | -------- |
+|  3300 |        4 |
+|  3450 |       62 |
+|  3600 |        4 |
+|  3900 |        4 |
+|  4050 |        2 |
+|  4200 |        2 |
+
+</div>
+
+* **OT Ser and V2306 Oph.** None of the V band images have flats with the
+proper focus position. Therefore, flats of two posterior nights (2015-04-05 and
+2015-04-07) were downloaded and used regardless of focus position.
+
+* **12 Oph.** This target has no comparison stars in the field of view and,
+therefore, it was supposed to have a reference exposure (12 Oph Ref) for each
+target exposure. However, reference images were obtained for only one of the
+two nights that 12 Oph was observed. In addition, the night with both target
+and reference images only contains 6 images of the target (of the 56 obtained)
+and 3 refence images.
+
+Considering the lack of flats with the proper focus, I have finally decided to
+use all the flats available, regardless of the focus position. 
 
 ## Reduction
 
@@ -32,8 +70,11 @@ IMGQS is a keyword inserted in the FITS header after ```gaia-imgqs``` that
 contains several quality indicators. All the science images used are valid and, 
 hence, the images with invalid IMGQS can be updated with 0099.
 
-All the images could be cleaned from bias, darks and flats (see [Flats
-availability below](#flats-availability)). However, no catalog could be produced for 4 images:
+All the images could be cleaned from bias, darks and flats ([see issues
+regarding flats](#data-issues)). However, no catalog could be
+produced for 4 images:
+
+<div align="center">
 
 | image | object |
 | ----- | ------ |
@@ -42,34 +83,47 @@ availability below](#flats-availability)). However, no catalog could be produced
 | 2457071/TJO2457071.70402_R_iml.fits.gz | 12 Oph |
 | 2457116/TJO2457116.45622_U_iml.fits.gz | OT Ser |
 
-## Flats availability
+</div>
 
-I have observed the following issues regarding flats availability:
+Therefore, the resulting reduction provided:
 
-* **HD 98712.** The focus position for this object is not constant and several 
-images were obtained with a different focus:
+<div align="center">
 
-| focus | images   |
-| ----- | -------- |
-|  3300 |        4 |
-|  3450 |       62 |
-|  3600 |        4 |
-|  3900 |        4 |
-|  4050 |        2 |
-|  4200 |        2 |
+| object     | night   | images | stars |
+| ---------- | ------- | ------ | ----- |
+| 12 Oph     | 2457071 |     47 |    64 |
+| 12 Oph     | 2457073 |      6 |    10 |
+| 12 Oph Ref | 2457073 |      3 |    47 |
+| HD 135599  | 2457032 |     14 |   125 |
+| HD 135599  | 2457033 |     14 |   107 |
+| HD 135599  | 2457034 |     14 |   131 |
+| HD 135599  | 2457036 |     14 |   135 |
+| HD 135599  | 2457037 |      4 |    24 |
+| HD 135599  | 2457045 |     12 |    73 |
+| HD 135599  | 2457046 |      6 |    33 |
+| HD 180617  | 2457073 |      4 |   386 |
+| HD 180617  | 2457081 |     15 |  1129 |
+| HD 98712   | 2457061 |     10 |    84 |
+| HD 98712   | 2457062 |      8 |    70 |
+| HD 98712   | 2457063 |     30 |   425 |
+| HD 98712   | 2457064 |     30 |   426 |
+| OT Ser     | 2457109 |     75 |  1416 |
+| OT Ser     | 2457110 |     75 |  1176 |
+| OT Ser     | 2457111 |     75 |   949 |
+| OT Ser     | 2457112 |     75 |  1308 |
+| OT Ser     | 2457113 |     75 |  1483 |
+| OT Ser     | 2457114 |     45 |   680 |
+| OT Ser     | 2457115 |     25 |   364 |
+| OT Ser     | 2457116 |     38 |   142 |
+| OT Ser     | 2457117 |     50 |   541 |
+| V2306 Oph  | 2457109 |     25 |  1101 |
+| V2306 Oph  | 2457110 |     25 |  1228 |
+| V2306 Oph  | 2457111 |     25 |  1036 |
+| V2306 Oph  | 2457112 |     27 |  1760 |
+| V2306 Oph  | 2457113 |     27 |  1399 |
+| V2306 Oph  | 2457114 |      7 |   184 |
+| V2306 Oph  | 2457115 |      7 |   188 |
+| V2306 Oph  | 2457117 |      7 |   146 |
 
-* **OT Ser and V2306 Oph.** None of the V band images have flats with the proper 
-focus position. Therefore, flats of two posterior nights 
-(2015-04-05 and 2015-04-07) were downloaded.
+</div>
 
-Considering the lack of flats with the proper focus, I have finally decided to
-use all the flats available, regardless of the focus position. 
-
-## 12 Oph
-
-This target has no comparison stars in the field of view and, therefore, it was 
-supposed to have a reference exposure (12 Oph Ref) for each target exposure. 
-However, reference images were obtained for only one of the two nights that 
-12 Oph was observed. In addition, the night with both target and reference 
-images only contains 6 images of the target (of the 56 obtained) and 3 refence 
-images.
--- light_curve.py
+++ light_curve.py
@@ -1,31 +1,31 @@
 """
-Plot light curves for all stars in a list of SExtractor files
+Construct light curves from photometry stored in database
 """
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-from re import findall
 import argparse
 import os.path
 
-import numpy as np
+# import numpy as np
 
-from bokeh.palettes import plasma
-from bokeh.models.annotations import Whisker, Span
-from bokeh.models.sources import ColumnDataSource
-from bokeh.resources import INLINE
-import bokeh.plotting as plt
+# from bokeh.palettes import plasma
+# from bokeh.models.annotations import Whisker, Span
+# from bokeh.models.sources import ColumnDataSource
+# from bokeh.resources import INLINE
+# import bokeh.plotting as plt
 
-from astropy.modeling import models, fitting
-from astropy.stats import histogram
-from astropy.units import arcsec
-from astropy.time import Time
-from astropy import table
+# from astropy.modeling import models, fitting
+# from astropy.stats import histogram
+# from astropy.units import arcsec
+# from astropy.time import Time
 import astropy.coordinates as coord
+from astropy import table
 
 from icat.logs import get_logger, LogOrProgressBar
+from icat.mysql_database import MySQLDatabase
 from icat.config import config_section
-from icat import system
+# from icat import system
 import icat.pipelines as ppl
 
 
@@ -40,197 +40,50 @@ def multiple_observations(tbl, keys):
     return len(tbl) > 1
 
 
-def run(input_catalogs, recursive=False, files_filter="trl.dat", max_sep=5,
-        max_chi=9):
+def run(ref_path=".", max_sep=5, max_chi=9, proposal=None, name=None):
     """
     Pipeline execution function
     """
 
-    cat_files = list(system.path_flatten(input_catalogs, recursive,
-                                         files_filter=files_filter))
+    log_msg = ""
+    log_msg += " for object '{}'".format(name) if name else ""
+    log_msg += " from proposal {}".format(proposal) if proposal else ""
 
-    logger.info("Reading {} catalogs...".format(len(cat_files)))
+    logger.info("Reading photometry%s...", log_msg)
 
-    max_len = 0
-    catalogs = []
-    ref_idx = None
-    for idx, cat in enumerate(LogOrProgressBar(cat_files)):
-        time = float(findall(r"\d+\.\d+", cat)[0])
-        cat_table = table.Table.read(cat, format="ascii.sextractor")
-        cat_table["JD"] = time
-        cat_table["Name"] = cat
-        catalogs += [cat_table]
-        cat_len = len(cat_table)
-        if cat_len > max_len:
-            max_len = cat_len
-            ref_idx = idx
+    db = MySQLDatabase(option_groups=["Database"])
 
-    if ref_idx is None:
-        raise ppl.PipelineError("No valid catalogs found!")
+    phot_columns = ["x_image", "y_image", "alpha_j2000", "delta_j2000"]
+    phot_columns += ["mag_auto", "magerr_auto", "fwhm_world", "flux_radius"]
+    phot_columns += ["ellipticity", "theta_j2000", "theta_world", "flags"]
+    im_columns = ["object", "jd", "filename"]
+
+    query = "SELECT {}, object, jd, filename FROM ph3_photometry "
+    query += "JOIN ph3_images ON ph3_images.id=ph3_photometry.image_id "
+    if proposal and name:
+        query += "WHERE propcode={} AND object='{}'".format(proposal, name)
+    elif proposal:
+        query += "WHERE propcode={}".format(proposal)
+    elif name:
+        query += "WHERE object='{}'".format(name)
+
+    result = db.execute(query.format(",".join(phot_columns)))
+
+    cat_table = table.Table(rows=result or None, names=phot_columns+im_columns)
+
+    if not cat_table:
+        raise ppl.PipelineError("No photometry found!")
 
     site = config_section("Location")
     OAdM = coord.EarthLocation(lat=site["latitude"], lon=site["longitude"],
                                height=site["elevation"])
 
-    ref_file = cat_files.pop(ref_idx)
-    ref_path = os.path.dirname(ref_file)
-    ref_catalog = catalogs.pop(ref_idx)
-    ref_time = int(ref_catalog["JD"][0])
-    ref_pos = coord.SkyCoord(ref_catalog["ALPHA_J2000"],
-                             ref_catalog["DELTA_J2000"])
-    jd = Time(ref_catalog["JD"], format="jd")
-    ref_altaz = ref_pos.transform_to(coord.AltAz(obstime=jd, location=OAdM))
-
-    log_msg = "Matching {} catalogs with '{}'..."
-    logger.info(log_msg.format(len(catalogs), ref_file))
-
-    num_stars = len(ref_catalog)
-    photometry = ref_catalog.copy()
-    photometry["Star"] = list(range(num_stars))
-    photometry["JD"] = photometry["JD"]
-    photometry["Airmass"] = ref_altaz.secz
-
-    all_seps = []
-    for cat in LogOrProgressBar(catalogs):
-        cat_pos = coord.SkyCoord(cat["ALPHA_J2000"], cat["DELTA_J2000"])
-        jd = Time(cat["JD"], format="jd")
-        altaz = cat_pos.transform_to(coord.AltAz(obstime=jd, location=OAdM))
-        idx, sep, _ = cat_pos.match_to_catalog_sky(ref_pos)
-        cat["Star"] = idx
-        cat["Airmass"] = altaz.secz
-        photometry = table.vstack([photometry, cat[sep < max_sep*arcsec]])
-        all_seps += sep.arcsec.tolist()
-
-    y, x = histogram(all_seps, bins="knuth")
-    hist = plt.figure()
-    hist.quad(top=y, bottom=0, left=x[:-1], right=x[1:])
-    hist.add_layout(Span(location=max_sep, dimension="height"))
-    hist_name = os.path.join(ref_path, "separation.html")
-    plt.save(hist, filename=hist_name, title="Matched stars separation",
-             resources=INLINE)
-
-    logger.info("Separation histogram saved in '%s'", hist_name)
-
-    light_curves = photometry.group_by("Star")
-    light_curves = light_curves.groups.filter(multiple_observations)
-
-    log_msg = "Selecting constant stars among %s light curves..."
-    logger.info(log_msg, len(light_curves.groups))
-
-    min_mag = light_curves["MAG_AUTO"] - light_curves["MAGERR_AUTO"]
-    max_mag = light_curves["MAG_AUTO"] + light_curves["MAGERR_AUTO"]
-    errors = dict(base=light_curves["Airmass"], lower=min_mag, upper=max_mag)
-    error_bars = ColumnDataSource(data=errors)
-
-    y_axis = (max(light_curves["MAG_AUTO"]) + 0.1,
-              min(light_curves["MAG_AUTO"]) - 0.1)
-
-    chi_plot = plt.figure(tools="hover,box_zoom,undo,redo,reset,save",
-                          y_range=y_axis)
-    chi_plot.add_layout(Whisker(source=error_bars, base="base",
-                                lower="lower", upper="upper"))
-    palette = plasma(num_stars)
-
-    ref_stars = {}
-    for star in LogOrProgressBar(light_curves.groups):
-        color = star["Star"][0]
-
-        chi_plot.line(star["Airmass"], star["MAG_AUTO"],
-                      line_color=palette[color])
-
-        linear_model = models.Linear1D(bounds={"slope": (0, 1)})
-        linear_fitter = fitting.LevMarLSQFitter()
-        weights = 1 / star["MAGERR_AUTO"]**2
-        linear_fit = linear_fitter(linear_model, star["Airmass"],
-                                   star["MAG_AUTO"], weights=weights)
-
-        chi_plot.line(star["Airmass"], linear_fit(star["Airmass"]),
-                      line_color=palette[color], line_dash="dashed")
-
-        residuals = (star["MAG_AUTO"] - linear_fit(star["Airmass"]))**2
-        num_obs = len(residuals)
-        chi2 = sum(weights * residuals) / (num_obs - 2) if num_obs > 2 else 0
-
-        logger.debug("Star %s (%s measurements): %s +/- %s * X (Chi2: %s)",
-                     color, num_obs, linear_fit.intercept.value,
-                     linear_fit.slope.value, chi2)
-
-        if 0 < chi2 < max_chi:
-            ref_stars[color] = num_obs
-
-    airmass_name = os.path.join(ref_path, "airmass.html")
-    plt.save(chi_plot, filename=airmass_name, title="Airmass",
-             resources=INLINE)
-
-    logger.info("Airmass curves saved in '%s'", airmass_name)
-
-    max_obs = max(ref_stars.values())
-    ref_stars = [key for key, value in ref_stars.iteritems()
-                 if value == max_obs]
-    num_standards = len(ref_stars)
-
-    logger.info("Computing differential magnitudes with %s standard stars",
-                len(ref_stars))
-
-    light_curves["Standard"] = [star in ref_stars
-                                for star in light_curves["Star"]]
-    light_curves["FLUX_AUTO"] = pow(10, -0.4*light_curves["MAG_AUTO"])
-
-    ref_mag = {}
-    ref_err = {}
-    grouped_cats = light_curves.group_by("Name")
-    for cat in LogOrProgressBar(grouped_cats.groups):
-        if np.count_nonzero(cat["Standard"]) != num_standards:
-            continue
-        ref_flux = cat["FLUX_AUTO"][cat["Standard"]]
-        ref_mag[cat["Name"][0]] = -2.5 * np.log10(sum(ref_flux))
-        ref_errflux = ref_flux**2 * cat["MAGERR_AUTO"][cat["Standard"]]**2
-        ref_err[cat["Name"][0]] = np.sqrt(sum(ref_errflux)) / sum(ref_flux)
-
-    mag_corr = []
-    magerr_corr = []
-    for row in light_curves:
-        mag_corr += [row["MAG_AUTO"] - ref_mag.get(row["Name"], 0)]
-        magerr_corr += [np.sqrt(row["MAGERR_AUTO"]**2 +
-                                ref_err.get(row["Name"], 0)**2)]
-
-    light_curves["MAG_CORR"] = mag_corr
-    light_curves["MAGERR_CORR"] = magerr_corr
-    light_curves["JD_CORR"] = light_curves["JD"] - ref_time
-
-    light_curves.sort("JD_CORR")
-    mask = [name in ref_mag.keys() for name in light_curves["Name"]]
-    light_curves = light_curves[mask].group_by("Star")
-
-    min_mag = light_curves["MAG_CORR"] - light_curves["MAGERR_CORR"]
-    max_mag = light_curves["MAG_CORR"] + light_curves["MAGERR_CORR"]
-    errors = dict(base=light_curves["JD_CORR"], lower=min_mag, upper=max_mag)
-    error_bars = ColumnDataSource(data=errors)
-
-    y_axis = (max(light_curves["MAG_CORR"]) + 0.1,
-              min(light_curves["MAG_CORR"]) - 0.1)
-
-    light_plot = plt.figure(tools="hover,box_zoom,undo,redo,reset,save",
-                            y_range=y_axis)
-    light_plot.add_layout(Whisker(source=error_bars, base="base",
-                                  lower="lower", upper="upper"))
-
-    for star in light_curves.groups:
-        color = star["Star"][0]
-
-        light_plot.line(star["JD_CORR"], star["MAG_CORR"],
-                        line_color=palette[color])
-
-    curves_name = os.path.join(ref_path, "light_curves.html")
-    plt.save(light_plot, filename=curves_name, title="Light curves",
-             resources=INLINE)
-
-    logger.info("Light curves saved in '%s'", curves_name)
-
     result_name = os.path.join(ref_path, "curves.html")
+    logger.info("Light curves saved in '%s'", result_name)
+
     if os.path.isfile(result_name):
         os.remove(result_name)
-    light_curves.write(result_name, format="jsviewer")
+    cat_table.write(result_name, format="jsviewer", max_lines=-1)
 
 
 def main():
@@ -240,15 +93,17 @@ def main():
 
     parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument("-r", "--recursive", action="store_true",
-                        help="recursively enter in directories")
+    parser.add_argument("path", default=".", nargs="*",
+                        help="path where to store results")
+
+    parser.add_argument("-p", "--proposal",
+                        help="select photometry from a single proposal")
+
+    parser.add_argument("-o", "--object",
+                        help="select photometry from a single object")
 
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="log debug messages")
-
-    help_msg = "when path is a directory, read only files having FILTER "
-    help_msg += "in name (default: '%(default)s')"
-    parser.add_argument("-f", "--filter", default="trl.dat", help=help_msg)
 
     help_msg = "maximum separation to match the same star in different "
     help_msg += "catalogs (in arc seconds)"
@@ -257,15 +112,12 @@ def main():
     help_msg = "maximum chi2 to consider a star to be constant"
     parser.add_argument("-c", "--chi2", default=9, help=help_msg)
 
-    parser.add_argument("path", nargs="+",
-                        help="file or directory with SExtractor files to use")
-
     args = parser.parse_args()
 
     logger = get_logger(__name__, verbose=args.verbose)
 
     try:
-        run(args.path, args.recursive, args.filter, args.separation, args.chi2)
+        run(args.path, args.separation, args.chi2, args.proposal, args.object)
     except Exception as err:
         logger.exception(err)
         raise
